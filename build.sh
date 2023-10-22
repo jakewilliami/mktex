9f1ae7a51be10393b001b8dee9aad633bd67109c
Jake Ireland <jakewilliami@icloud.com>
2023-10-22 15:18:39 +1300

    Move binary project to root dir
    When migrating git history, the directory location is migrated across
    too.  Had to move it to the root of this repo.

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
-set -xe
+#!/usr/bin/env sh
 
+set -xe
+trap 'exit 1' INT
+
+# Build programme
 MODE="release"
 cargo build --$MODE
 D="$(basename "$PWD")"
 F="${D%%.*}"
 strip ./target/$MODE/$F
 cp -f ./target/$MODE/$F ./
+
