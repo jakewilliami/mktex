6ad9ddd4b7f96ce77707d8b067b0f4ee2ab1fb16
Jake Ireland <jakewilliami@icloud.com>
2024-05-14 23:17:57 +1200

    Change base file paths for TeX macros after refactor
    See jakewilliami/tex-macros#51

--- src/config.rs
+++ src/config.rs
@@ -4,16 +4,16 @@ pub const GITHUB_REPO_NAME: &str = "tex-macros";
 pub const MAIN_BRANCH: &str = "master";
 
 // Base Resource Path
-pub const RESOURCE_PARENT: &str = "general_macros/class/";
+pub const RESOURCE_PARENT: &str = "class/";
 
 // Arteacle Resources
-pub const CLS_RESOURCE: &str = "general_macros/class/arteacle.cls";
-pub const TMPL_RESOURCE: &str = "templates/template_Class_Typesetting.tex";
+pub const CLS_RESOURCE: &str = "class/arteacle.cls";
+pub const TMPL_RESOURCE: &str = "templates/arteacle.tex";
 
 // Teamer Resources
-pub const BMR_RESOURCE: &str = "general_macros/class/teamer.cls";
-pub const BMR_TMPL_RESOURCE: &str = "templates/template_Class_Beamer.tex";
-pub const BMR_THEME_PATH: &str = "general_macros/class/teamer/";
+pub const BMR_RESOURCE: &str = "class/teamer.cls";
+pub const BMR_TMPL_RESOURCE: &str = "templates/teamer.tex";
+pub const BMR_THEME_PATH: &str = "class/teamer/";
 pub const BMR_THEME_COLOUR: &str = "beamercolorthemetea.sty";
 pub const BMR_THEME_INNER: &str = "beamerinnerthemetea.sty";
 pub const BMR_THEME_OUTER: &str = "beamerouterthemetea.sty";
